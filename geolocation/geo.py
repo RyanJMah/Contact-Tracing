0982a394cd70cec51265d376e29f6d017075c198
Norman <norman.x.zhang@gmail.com>
2020-11-07 15:57:27 -0700

    ryan is too cool. i added basically nothing im just testing shit out

--- geolocation/geo.py
+++ geolocation/geo.py
@@ -1,1 +1,10 @@
-print("Hello!")
+
+import json
+
+
+import urllib.request
+contents = urllib.request.urlopen("https://api.ipgeolocation.io/ipgeo?apiKey=aaaf819d25a84c3c84f09ac62f39c8d8").read()
+
+print(contents)
+
+print("fuck this")
