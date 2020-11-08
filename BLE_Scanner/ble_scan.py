848609d21e837839728c51bb12be47cf0ddf6632
RyanJMah <rmah@uwaterloo.ca>
2020-11-07 22:02:10 -0700

    was able to obtain uuid and rssi

--- BLE_Scanner/ble_scan.py
+++ BLE_Scanner/ble_scan.py
@@ -1,65 +1,68 @@
 import os
 import re
-import threading
 import queue
+import threading
 import subprocess
 
-class _scan_BLE_thread(threading.Thread):
+class BLE_Tracker(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
 
-		self.out_pipe = queue.Queue()	
+		self.p1 = None
+		self.p2 = None
+
 		self.running = True
-		self.daemon = True
+		self.out_buffer = queue.Queue()
 
 	def _decode_response(self, response):
-		ret = re.findall("Device .*RSSI.*\n", response).group()
+		ret = re.findall("Device .*RSSI.*\n", response)
 		ret = [s[:len(s)-1] for s in ret]
-		return ret		
+		return ret
+
+	def start_subprocesses(self):
+		self.p1 = subprocess.Popen(
+			["bluetoothctl", "scan", "on"],
+			shell = True,
+			stdout = subprocess.PIPE
+		)
+		self.p2 = subprocess.Popen(["bluetoothctl scan on"], shell = True, stdout = subprocess.PIPE)
+
+	def kill_subprocesses(self):
+		self.p1.terminate()
+		self.p1.wait()
+		self.p2.terminate()
+		self.p2.wait()
 
 	def run(self):
-		os.system("bluetoothctl > pipe")
-		while(True):
+		self.start_subprocesses()
+
+		for r in iter(self.p1.stdout.readline, ""):
 			if not(self.running):
 				break
-
-			with open("pipe", "r") as f:
-				response = self._decode_response(f.read())
-
-			self.out_pipe.put(response)
-
-			with open("pipe", "w") as f:
-				f.write("")
-
-
-class BLE_Tracker():
-	def __init__(self):
-		self.scanner_thread = None
-
-	def get_device_info(self):
-		if self.scanner_thread is None:
-			throw(ValueError)
-		else:
-			while self.scanner_thread.out_pipe.is_empty():
-				pass
-
-			self.scanner_thread.out_pipe.get()
-
-	def start(self):
-		self.scanner_thread = _scan_BLE_thread()
-		self.scanner_thread.start()
+			
+			response = r.decode()
+			if self._decode_response(response) != []:
+				self.out_buffer.put(self._decode_response(response))
 
 	def stop(self):
-		self.scanner_thread.running = False
+		self.kill_subprocesses()
+		self.running = False
 
 
 def main():
 	tracker = BLE_Tracker()
 	tracker.start()
 
-	while True:
-		print(tracker.get_device_info())
+	try:
+		while True:
+			if not(tracker.out_buffer.empty()):
+				print(tracker.out_buffer.get())
 
+	except KeyboardInterrupt:
+		tracker.stop()
+
+
+	exit()
 
 
 if __name__ == "__main__":
