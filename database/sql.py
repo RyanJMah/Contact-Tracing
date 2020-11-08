390d05d26c1a275e7b9e8a1b5327dba382ff6299
rob-villarre <rob.villarre@gmail.com>
2020-11-07 17:13:32 -0700

    function prototypes

--- database/sql.py
+++ database/sql.py
@@ -1,16 +1,24 @@
 import mysql.connector
 import pandas as pd
 
+def add_user():
+    return
+def read_user():
+    return
+def add_incidents():
+    return
+def read_incidents():
+    return
+
 def main():
     db = mysql.connector.connect(
         host = "34.67.23.158",
         user = "root",
-        password = "UwU",
+        password = "",
         database = "db"
     )
     
     df = pd.read_sql("SELECT * FROM users", db)
     print(df)
-
 if __name__ == "__main__":
     main()
